4e79d156fda8d6f681a7cc11f205a3c0e6f66299
Alfred Tuva <muindetuva@gmail.com>
2022-04-25 12:49:30 +0300

    Using route params

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -16,6 +17,10 @@ const router = createRouter({
     {
       path: '/users',
       component: UsersList,
+    },
+    {
+      path: '/teams/:id',
+      component: TeamMembers,
     },
   ],
 });
