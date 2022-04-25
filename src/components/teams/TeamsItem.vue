4f785d019d3eec85cd49fdfa18798788a8fbf241
Alfred Tuva <muindetuva@gmail.com>
2022-04-25 15:07:46 +0300

    Use named routes

--- src/components/teams/TeamsItem.vue
+++ src/components/teams/TeamsItem.vue
@@ -11,7 +11,11 @@ export default {
   props: ['name', 'memberCount', 'id'],
   computed: {
     teamMembersLink() {
-      return '/teams/' + this.id;
+      // return '/teams/' + this.id;
+      return {
+        name: 'team-members',
+        params: { teamId: this.id },
+      };
     },
   },
 };
